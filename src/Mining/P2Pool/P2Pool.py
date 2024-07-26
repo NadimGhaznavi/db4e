107bad11afe8a753e12c858d1d5cfd778a62c56f
NadimGhaznavi <nghaznavi@gmail.com>
2024-07-26 18:53:26 -0400

    typo

--- src/Mining/P2Pool/P2Pool.py
+++ src/Mining/P2Pool/P2Pool.py
@@ -17,7 +17,7 @@ for project_dir in project_dirs:
 
 # Import required db4e modules.
 from Db4eStartup.Db4eStartup import Db4eStartup
-from MiningMongoDb.MiningMongoDb import MiningMongoDb
+from MiningDb.MiningDb import MiningDb
 from BlockFoundEvent.BlockFoundEvent import BlockFoundEvent
 from ShareFoundEvent.ShareFoundEvent import ShareFoundEvent
 from XMRTransaction.XMRTransaction import XMRTransaction
